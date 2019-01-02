cf4ce5458b96b54f3709a081927a7dfd97a04bb0
wangpengz <wangpengz@yonyou.com>
2019-01-02 18:18:01 +0800

    init

---
+++ src/main/java/com/yonyougov/servertwo/ServerTwoApp.java
@@ -0,0 +1,13 @@
+package com.yonyougov.servertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTwoApp {
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(ServerTwoApp.class, args);
+    }
+
+}
